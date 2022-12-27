aaaeeb4d0cd2934b0efbf55f460003d59beeb632
lebrancconvas <lebrancconvas@gmail.com>
2022-12-27 08:21:21 +0700

    ✨ feat: edit webpage

---
+++ app/src/pages/test/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next';
+import Head from 'next/head'; 
+
+const Test: NextPage = () => {
+	return(
+		<div>
+			<Head>
+				<title>Test Webpage</title> 
+			</Head>
+			<h1>Hello Test</h1>
+		</div>
+	)
+}
+
+export default Test; 
